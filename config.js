d1b440ad45ec5c9cfcd5d372d77ca87cc2b28da9
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-05-06 13:47:00 -0400

    update config.js pt2

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const newsApiKey = "f21c4734cbcd4f5292b1f63ee57ef784";
-const stocksApiKey = "XVKZU3RCSV6M76TW";
+const newsApiKey = "f21c4734cbcd4f5292b1f63ee57ef784" 
+const stockApiKey = "XVKZU3RCSV6M76TW";
