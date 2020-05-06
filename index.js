801ccb442e7ff7368a1d3592fa45fcbfaddc3595
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-05-06 07:00:41 -0400

    adding weather card to footer, will need styling

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+const weatherContainer = document.querySelector(".weather-container");
+let weatherData;
 
-// let searchZip= document.getElementById("search")
+$(function () {
+    axios.get("http://api.weatherstack.com/current?access_key=1b963aaec64922ece9101f2139f57884&query=Atlanta&units=f")
+    .then(data => {
+        console.log(data.data.current);
+        weatherData = data.data.current;
+        weatherContainer.innerHTML = renderWeather(weatherData);
+    })
+})
 
 
-
-// function getCityName(){   
-//     e.preventDefault()
-//     let zipcode = document.querySelector(".form-control").value
-//     axios.get(`http://ZiptasticAPI.com/${zipcode}`).then(data => { 
-//         return data.data.city;})
-// } 
-// searchZip.addEventListener("click",getCityName) 
-
-
+const renderWeather = function(data) {
+    for (const property in weatherData) {
+        console.log(`${property}: ${weatherData[property]}`);
+        console.log(weatherData["weather_icons"][0]);
+        console.log(weatherData);
+        return x = `<div class="movie col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+        <div class="card" style="width: 100%;">
+            <img src=${weatherData["weather_icons"][0]} id="0" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Temp: ${weatherData["temperature"]}</h5>
+                <p class="card-text">${weatherData["weather_descriptions"][0]}</p>
+                <p class="card-text">Precip: ${weatherData["precip"]}%</p>
+                <p class="card-text">Humidity: ${weatherData["humidity"]}</p>
+                <p class="card-text">Wind Speed: ${weatherData["wind_speed"]}mph</p>
+            </div>
+        </div>
+    </div>`
+    };
+};
