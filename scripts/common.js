744039ef0b31365e1cd7e0de15aa1468acb33304
Park Jack <parker.jackson.hhs@gmail.com>
2020-05-07 14:03:27 -0400

    added the news into the main container instead of the footer

--- scripts/common.js
+++ scripts/common.js
@@ -5,7 +5,7 @@ let newsData;
 const renderNews = function (newsArray) {
     let newsHtmlArray = newsArray.map(function (currentStory, i) {
         return `<div class="movie col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-                        <div class="card" style="width: 100%;">
+                        <div class="card">
                         <a href="${currentStory.url}"><img src=${currentStory.urlToImage} id="0" class="card-img-top" alt="${currentStory.title}"></a>
                         <div class="card-body">
                                 <h5 class="card-title">${currentStory.title}</h5>
@@ -20,11 +20,11 @@ const renderNews = function (newsArray) {
     return newsHtmlArray.join("");
 };
 
-let newsApiKey = "f21c4734cbcd4f5292b1f63ee57ef784";
+// let newsApiKey = "f21c4734cbcd4f5292b1f63ee57ef784";
 window.addEventListener("DOMContentLoaded", function () {
     myForm.addEventListener("submit", function (e) {
         e.preventDefault();
-        let zipcode = document.querySelector(".form-control").value;
+        let zipcode = document.querySelector("#search_bar").value;
         axios.get(`http://ZiptasticAPI.com/${zipcode}`).then(data => {
             let $searchString = data.data.city;
             let urlEncodedSearchString = encodeURIComponent($searchString)
